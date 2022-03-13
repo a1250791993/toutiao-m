7fcfdd7e79437f7f62677afd74bbafa6b1f4cc12
zhoulingyun <1250791993@qq.com>
2022-03-13 12:57:02 +0800

    夏目初始化-引入了vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <router-view></router-view>
     <h2>黑马程序员</h2>
+    <i class="iconfont icon-dianzan2"></i>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import store from './store'
 // 加载全局样式
 import '@/styles/index.less'
 
+// 引入vant组件(全部组件)
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
